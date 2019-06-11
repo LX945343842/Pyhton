4040e0164db585422bf02e87cd8d9ad5ff165475
YSadie <47059594+YSadie@users.noreply.github.com>
2019-06-11 19:16:07 +0800

    Update settings.py

--- spider/spider/settings.py
+++ spider/spider/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'spider',
         'USER': 'root',
-        'PASSWORD': 'yst202',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
 
